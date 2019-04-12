a1a6ce128ae0db58465d6db244394e39cf72548d
xfmax <xufengbase@gmail.com>
2019-04-12 21:33:02 +0800

    update

--- MyApplication/mvvm/src/main/java/com/example/mvvm/UserProfileViewModel.java
+++ MyApplication/mvvm/src/main/java/com/example/mvvm/UserProfileViewModel.java
@@ -1,4 +1,22 @@
 package com.example.mvvm;
 
-public class UserProfileViewModel {
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+public class UserProfileViewModel extends ViewModel {
+    private String userId;
+    private MutableLiveData<User> user;
+
+    public void init(String userId){
+        this.userId = userId;
+        user = new MutableLiveData<>();
+    }
+
+    public void refresh(String userId){
+        user = new MutableLiveData<>();
+    }
+
+    public MutableLiveData<User> getUser(){
+        return user;
+    }
 }
